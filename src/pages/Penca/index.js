14ffbd8674a769c6f8a466aada3105dc5e69c21c
Santiago Noguera <santiago.noguera@moove-it.com>
2022-10-12 22:08:12 -0300

    Release 12/10/2022 v5

--- src/pages/Penca/index.js
+++ src/pages/Penca/index.js
@@ -38,7 +38,7 @@ const Penca = () => {
       message: 'Penca DTA 2022',
       duration: 30,
       description:
-        'Completa la penca eligiendo la fecha que deseas pronosticar. Puedes pronosticar todas fechas ahora, o hacerlo fecha a fecha.',
+        'Completa la penca eligiendo la fecha que deseas pronosticar. Puedes pronosticar todas fechas ahora, o hacerlo fecha a fecha',
       onClick: () => {
         console.log('Notification Clicked!');
       },
